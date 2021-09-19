10c2fefeb8f23540802e02938343e6b045065d2d
Adam Spotton <aspotton@gmail.com>
2021-09-18 22:28:14 -0400

    Added Hero; added/use Hero image from https://phaser.io/tutorials/making-your-first-phaser-3-game/

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-var game = null;
 var platforms = null;
 
 var config = {
@@ -24,6 +23,31 @@ var config = {
 function preload() {
     this.load.image('background', 'images/background.png');
 
+    this.load.spritesheet('hero',
+        'images/phaser.io/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('hero', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'turn',
+        frames: [ { key: 'hero', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('hero', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
     this.load.image('ground', 'images/ground.png');
     this.load.image('grass:8x1', 'images/grass_8x1.png');
     this.load.image('grass:6x1', 'images/grass_6x1.png');
@@ -44,14 +68,36 @@ function loadLevel (data, game) {
     platforms = game.physics.add.staticGroup();
 
     data.platforms.forEach(spawnPlatform, this);
+
+    spawnCharacters({hero: data.hero}, game);
 };
 
 function spawnPlatform (platform, game) {
     //game.add.sprite(platform.x, platform.y, platform.image);
-    platforms.create(platform.x, platform.y, platform.image).setOrigin(0, 0);
+    platforms.create(platform.x, platform.y, platform.image).setOrigin(0, 0).refreshBody();
+}
+
+function spawnCharacters (data, game) {
+    // spawn hero
+    player = game.physics.add.sprite(data.hero.x, data.hero.y, 'hero');
+    game.physics.add.collider(player, platforms);
+
+    player.setCollideWorldBounds(true);
+
+    //game.add.existing(player);
+
+    // game.hero = new Hero(game, data.hero.x, data.hero.y);
+    // game.add.existing(game.hero);
+    // game.physics.add.existing(game.hero);
 };
 
 window.onload = function () {
     game = new Phaser.Game(config);
 };
 
+class Hero extends Phaser.Physics.Arcade.Sprite {
+    constructor(game, x, y) {
+        console.log(x, y);
+        super(game.scene, x, y, 'hero');
+    }
+}
